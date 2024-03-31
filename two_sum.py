843a10344c0983ce7a9093609f9aaec6047ae856
Phạm Công Minh <76472728+wiuy3120@users.noreply.github.com>
2024-03-31 20:34:46 +0700

    init code formatter

--- two_sum.py
+++ two_sum.py
@@ -2,15 +2,16 @@
 
 from typing import List
 
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        result = [i for i, num in enumerate(nums) if num*2 == target]
+        result = [i for i, num in enumerate(nums) if num * 2 == target]
         if len(result) == 2:
             return result
-        
+
         result = []
-        target_nums = set(target - num for num in nums if num*2 != target)
-        
+        target_nums = set(target - num for num in nums if num * 2 != target)
+
         for i, value in enumerate(nums):
             if value in target_nums:
                 result.append(i)
