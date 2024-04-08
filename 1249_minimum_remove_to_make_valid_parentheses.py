010c3d4b652223a5c6abcc2a76edcb2bbf394f23
Phạm Công Minh <76472728+wiuy3120@users.noreply.github.com>
2024-04-09 00:57:35 +0700

    [1700]

--- 1249_minimum_remove_to_make_valid_parentheses.py
+++ 1249_minimum_remove_to_make_valid_parentheses.py
@@ -57,6 +57,22 @@ class Solution:
 
         return final_output
 
+    def minRemoveToMakeValid(self, s: str) -> str:
+        # convert string to list
+        s = list(s)
+        stack = []
+        for i, char in enumerate(s):
+            if char == "(":
+                stack.append(i)
+            elif char == ")":
+                if stack:
+                    stack.pop()
+                else:
+                    s[i] = ""
+        while stack:
+            s[stack.pop()] = ""
+        return "".join(s)
+
 
 s = "lee(t(c)o)de)"
 print(Solution().minRemoveToMakeValid(s))
