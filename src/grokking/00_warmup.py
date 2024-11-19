4ccd1fffa369944b5b3941a510ab976554ce82c0
minhpc <76472728+wiuy3120@users.noreply.github.com>
2024-11-19 22:20:44 +0700

    style: format code

--- src/grokking/00_warmup.py
+++ src/grokking/00_warmup.py
@@ -3,14 +3,8 @@ import bisect
 import math
 import random
 from collections import Counter, deque
-from heapq import (
-    heapify,
-    heappop,
-    heappush,
-    heappushpop,
-    heapreplace,
-    nlargest,
-)
+from heapq import (heapify, heappop, heappush, heappushpop, heapreplace,
+                   nlargest)
 from itertools import accumulate
 from typing import Dict, List, Optional, Set, Tuple
 
