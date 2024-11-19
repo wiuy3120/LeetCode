9efa4b3ff916afc4c82cd03150ec4bf79500e625
minhpc <76472728+wiuy3120@users.noreply.github.com>
2024-11-19 22:15:56 +0700

    medium(first): [2461]

---
+++ src/v2/medium.py
@@ -0,0 +1,66 @@
+# pyright: reportRedeclaration=false
+import bisect
+import math
+import random
+from collections import Counter, deque
+from heapq import (
+    heapify,
+    heappop,
+    heappush,
+    heappushpop,
+    heapreplace,
+    nlargest,
+)
+from itertools import accumulate
+from typing import Dict, List, Optional, Set, Tuple
+
+from sortedcontainers import SortedList
+
+# Binary Search
+
+
+# 2070. Most Beautiful Item for Each Query
+class Solution:
+    def maximumBeauty(
+        self, items: List[List[int]], queries: List[int]
+    ) -> List[int]:
+        from bisect import bisect
+
+        sorted_items = [[0, 0]] + sorted(items)
+
+        for i, item in enumerate(sorted_items[1:]):
+            item[1] = max(item[1], sorted_items[i][1])
+
+        return [
+            sorted_items[bisect(sorted_items, [query + 1]) - 1][1]
+            for query in queries
+        ]
+
+
+# 2461. Maximum Sum of Distinct Subarrays With Length K
+class Solution:
+    def maximumSubarraySum(self, nums: List[int], k: int) -> int:
+        counter = Counter(nums[:k])
+        distinct_count = len(counter)
+        cur_sum = sum(nums[:k])
+        res = 0
+        if distinct_count == k:
+            res = cur_sum
+
+        for i, num in enumerate(nums[:-k]):
+            counter[num] -= 1
+            if counter[num] == 0:
+                distinct_count -= 1
+
+            next_num = nums[i + k]
+            if counter[next_num] == 0:
+                distinct_count += 1
+                counter[next_num] = 1
+            else:
+                counter[next_num] += 1
+
+            cur_sum += next_num - num
+            if distinct_count == k:
+                res = max(res, cur_sum)
+
+        return res
