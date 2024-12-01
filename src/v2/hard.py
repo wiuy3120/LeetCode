f547531ce6d630d771b1fedb0e455e3a50ed1658
peri <76472728+wiuy3120@users.noreply.github.com>
2024-12-01 22:34:54 +0700

    hard(first): 2097

--- src/v2/hard.py
+++ src/v2/hard.py
@@ -2,7 +2,7 @@
 import bisect
 import math
 import random
-from collections import Counter, deque
+from collections import Counter, deque, defaultdict
 from heapq import (
     heapify,
     heappop,
@@ -87,3 +87,29 @@ class Solution:
                     return new_weight
                 heappush(priority_queue, (new_weight, row, col))
                 visited_cells[row][col] = True
+
+    # 2097. Valid Arrangement of Pairs
+    def validArrangement(self, pairs: List[List[int]]) -> List[List[int]]:
+        graph = defaultdict(list)
+        degree = defaultdict(int)  # net out degree
+        for x, y in pairs:
+            graph[x].append(y)
+            degree[x] += 1
+            degree[y] -= 1
+
+        for k in degree:
+            if degree[k] == 1:
+                x = k
+                break
+
+        ans = []
+
+        def fn(x):
+            """Return Eulerian path via dfs."""
+            while graph[x]:
+                fn(graph[x].pop())
+            ans.append(x)
+
+        fn(x)
+        ans.reverse()
+        return [[ans[i], ans[i + 1]] for i in range(len(ans) - 1)]
