b4b80d3436791f52236b838384a391df36611e5c
peri <76472728+wiuy3120@users.noreply.github.com>
2024-12-09 22:06:00 +0700

    medium(first): 3152

--- src/v2/medium.py
+++ src/v2/medium.py
@@ -3,14 +3,8 @@ import bisect
 import math
 import random
 from collections import Counter, deque
-from heapq import (
-    heapify,
-    heappop,
-    heappush,
-    heappushpop,
-    heapreplace,
-    nlargest,
-)
+from heapq import (heapify, heappop, heappush, heappushpop, heapreplace,
+                   nlargest)
 from itertools import accumulate
 from typing import Dict, List, Optional, Set, Tuple
 
@@ -115,3 +109,92 @@ class Solution:
                 return False
 
         return True
+
+    # 1760. Minimum Limit of Balls in a Bag
+    def minimumSize(self, nums: List[int], maxOperations: int) -> int:
+        heap = [(-num, -num, 1) for num in nums]
+        heapify(heap)
+        for _ in range(maxOperations):
+            max_item = heap[0]
+            num, divisor = -max_item[1], max_item[2]
+            heappushpop(
+                heap, (-math.ceil(num / (divisor + 1)), -num, divisor + 1)
+            )
+        return -heap[0][0]
+
+    def minimumSize(self, nums: List[int], maxOperations: int) -> int:
+        left, right = 0, max(nums)
+
+        while left < right:
+            mid = (left + right) // 2
+            num_ops = sum((num - 1) // mid for num in nums)
+
+            if num_ops > maxOperations:
+                left = mid + 1
+            else:
+                right = mid
+
+        return left
+
+    # 2054. Two Best Non-Overlapping Events
+    def maxTwoEvents(self, events: List[List[int]]) -> int:
+
+        def acc_max_func(last: Tuple[int, int], current: Tuple[int, int]):
+            return (
+                current[0],
+                max(last[1], current[1]),
+            )
+
+        start_times = list(
+            accumulate(
+                sorted(
+                    [(event[0], event[2]) for event in events], reverse=True
+                ),
+                acc_max_func,
+            )
+        )[::-1]
+        print(start_times)
+        end_times = list(
+            accumulate(
+                sorted([(event[1], event[2]) for event in events]),
+                acc_max_func,
+            )
+        )
+        print(end_times)
+        max_sum = 0
+        for end_time, value in end_times:
+            idx = bisect.bisect(start_times, end_time, key=lambda x: x[0])
+            if idx == len(start_times):
+                max_sum = max(max_sum, end_times[-1][1])
+                break
+            max_sum = max(max_sum, value + start_times[idx][1])
+
+        return max_sum
+
+    # 3152. Special Array II
+    def isArraySpecial(
+        self, nums: List[int], queries: List[List[int]]
+    ) -> List[bool]:
+        boundary_indices = [
+            i for i in range(len(nums) - 1) if (nums[i] + nums[i + 1]) % 2 == 0
+        ] + [len(nums) - 1]
+
+        def is_special(query: Tuple[int, int]):
+            start_idx = bisect.bisect_left(boundary_indices, query[0])
+            boundary = boundary_indices[start_idx]
+            return query[1] <= boundary
+
+        return [is_special(query) for query in queries]
+
+    def isArraySpecial(
+        self, nums: List[int], queries: List[List[int]]
+    ) -> List[bool]:
+        part_counter = 0
+        parts = [0]
+
+        for i, num in enumerate(nums[1:]):
+            if (num + nums[i]) % 2 == 0:
+                part_counter += 1
+            parts.append(part_counter)
+
+        return [parts[start] == parts[end] for start, end in queries]
