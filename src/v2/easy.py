e510262deebc8d8ee8c9da4a8d815f5b714797ef
peri <76472728+wiuy3120@users.noreply.github.com>
2025-01-01 14:58:30 +0700

    easy: 1422

--- src/v2/easy.py
+++ src/v2/easy.py
@@ -32,3 +32,39 @@ class Solution:
                 return i + 1
 
         return -1
+
+    # 1422. Maximum Score After Splitting a String
+    def maxScore(self, s: str) -> int:
+        n = len(s)
+        zero_counter = 0
+        max_score = 0
+        total_zeros = sum([1 for char in s if char == "0"])
+        for i, char in enumerate(s[:-1]):
+            if char == "0":
+                zero_counter += 1
+            max_score = max(
+                max_score, n - i - 1 - total_zeros + 2 * zero_counter
+            )
+        return max_score
+
+    def maxScore(self, s: str) -> int:
+        zero_counter = 0
+        max_score = 0
+        total_ones = sum([1 for char in s if char == "1"])
+        for i, char in enumerate(s[:-1]):
+            if char == "0":
+                zero_counter += 1
+            max_score = max(max_score, total_ones - i - 1 + 2 * zero_counter)
+        return max_score
+
+    def maxScore(self, s: str) -> int:
+        zero_counter, one_counter = 0
+        max_score = -1
+        for i, char in enumerate(s[:-1]):
+            if char == "0":
+                zero_counter += 1
+            else:
+                one_counter += 1
+            max_score = max(max_score, 2 * zero_counter - i - 1)
+        one_counter += 1 if s[-1] == "1" else 0
+        return max_score + one_counter
